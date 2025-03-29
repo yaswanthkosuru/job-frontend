0bf15f71a7bfb3855d6e853031617fc0a5c6bf0e
Yaswanth Kosuru <yaswanthkosuru999@gmail.com>
2025-03-29 15:08:55 +0530

    initial commit

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8000";
+export const API_URL = "https://yaswanthkosuru-job-backend.hf.space/";
 export const getAuthHeaders = () => {
     const token = localStorage.getItem("token");
     return {
