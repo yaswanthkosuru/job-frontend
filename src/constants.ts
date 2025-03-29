41e96b87acb151bbae59327a73ccbbd4265f0a8c
Yaswanth Kosuru <yaswanthkosuru999@gmail.com>
2025-03-29 15:32:04 +0530

    feat: fix change backend url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "https://yaswanthkosuru-job-backend.hf.space/";
+export const API_URL = "https://yaswanthkosuru-job-backend.hf.space";
 export const getAuthHeaders = () => {
     const token = localStorage.getItem("token");
     return {
