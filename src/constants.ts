1f03cab7c8a29fee4e1f41506834410cee95ecb2
Yaswanth Kosuru <yaswanthkosuru999@gmail.com>
2025-04-03 12:23:01 +0530

    feat: fix erros

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-// export const API_URL = "https://yaswanthkosuru-job-backend.hf.space";
-export const API_URL = "http://localhost:8000";
+export const API_URL = "https://yaswanthkosuru-job-backend.hf.space";
+// export const API_URL = "http://localhost:8000";
 export const getAuthHeaders = () => {
     const token = localStorage.getItem("token");
     return {
