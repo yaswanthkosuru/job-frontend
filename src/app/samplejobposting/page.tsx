0a82774ec33a62c117f85318d433a59a5426f543
Yaswanth Kosuru <yaswanthkosuru999@gmail.com>
2025-05-23 16:34:43 +0530

    feat: add sample job posting

---
+++ src/app/samplejobposting/page.tsx
@@ -0,0 +1,9 @@
+import JobPostingForm from "@/components/jobpostingForm";
+
+export default function JobPostingPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <JobPostingForm />
+    </div>
+  );
+}
